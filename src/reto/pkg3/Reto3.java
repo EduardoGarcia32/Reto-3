f8d0119b0f3200663bbac323bf37d93fdd505113
EduardoGarcia32 <slender@gmail.com>
2019-02-05 08:13:11 -0600

    Creacion de objetos y la solicitud de puntajes de objetos

--- src/reto/pkg3/Reto3.java
+++ src/reto/pkg3/Reto3.java
@@ -5,6 +5,10 @@
  */
 package reto.pkg3;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author AlumnoUTM
@@ -14,8 +18,24 @@ public class Reto3 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+    
+    public static void main(String[] args) throws IOException {
+        int puntaje;
+        Constructor personaje1 = new Constructor();
+        Mago personaje2 = new Mago();
+        Guerrero personaje3 = new Guerrero();
+        System.out.println("Indique puntaje para Construtor");
+        puntaje = Integer.parseInt(entrada.readLine());
+        personaje1.setPuntaje(puntaje);
+        
+        System.out.println("Indique puntaje para Mago");
+        puntaje = Integer.parseInt(entrada.readLine());
+        personaje2.setPuntaje(puntaje);
+        
+        System.out.println("Indique puntaje para Guerrero");
+        puntaje = Integer.parseInt(entrada.readLine());
+        personaje3.setPuntaje(puntaje);
     }
     
 }
